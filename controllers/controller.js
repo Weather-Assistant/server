b31f7a69ec5a6f07d705b382108bfd8ae662a2cb
muhafaa <muhafa99@gmail.com>
2020-01-10 01:11:28 +0700

    menambah parameter month di controller getCalender, agar hanya mengambil data bulan ini saja

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,7 +3,7 @@ const calendarificAPI = 'https://calendarific.com/api/v2'
 class CalendarifiController {
     static getCalender(req, res, next) {
         let key = process.env.CALENDARIFIC_KEY
-        axios.get(`${calendarificAPI}/holidays?api_key=${key}&country=${req.body.country}&year=${req.body.year}`)
+        axios.get(`${calendarificAPI}/holidays?api_key=${key}&country=${req.body.country}&year=${req.body.year}&month=1`)
             .then(({ data }) => {
                 res.status(200).json(data)
             }).catch((err) => {
