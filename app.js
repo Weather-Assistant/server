d4bcace90805bc24823496414e9799c7fda0f962
Richard Khonan <richardkhonan0797@gmail.com>
2020-01-10 14:37:42 +0700

    checkout

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express();
-const port = 3003;
+const port = 3000;
 const cors = require('cors');
 const router = require('./routes');
 const dotenv = require('dotenv').config();
 
 mongoose
-  .connect(process.env.DATABASE)
+  .connect(process.env.DATABASE,{useNewUrlParser: true})
   .then(() => {
     app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
-app.use('/', router)
     app.use('/', router);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
     app.listen(port, () =>
       console.log(`Example app listening on port ${port}!`)
     );
