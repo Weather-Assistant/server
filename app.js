042f29682088ddc3cc6d9b1e479f36b244a6ac04
Richard Khonan <richardkhonan0797@gmail.com>
2020-01-10 13:33:18 +0700

    perbaikan pixabay

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -21,3 +22,21 @@ mongoose
     );
   })
   .catch(err => console.error(`Connection error: ${err.message}`));
+=======
+const dotenv = require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const mongoose = require('mongoose')
+const cors = require('cors')
+const routes = require('./routes/index')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use('/',routes)
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+})
+>>>>>>> perbaikan pixabay
